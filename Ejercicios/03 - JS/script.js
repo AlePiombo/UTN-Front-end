46600522c8fe5d7c032318f581d5c2936622c508
AlePiombo <piomboalejandro@gmail.com>
2025-05-18 18:40:55 -0300

    Tarea1 JS

---
+++ Ejercicios/03 - JS/script.js
@@ -0,0 +1,35 @@
+// Creación de Variables
+
+let nombre = "Ana"
+let edad = 29
+let precio = 95.99
+
+console.log(nombre)
+console.log(edad)
+console.log(precio)
+
+
+//Modificación de variables
+edad += 1
+
+console.log("la edad ahora es " + edad)
+
+precio *= 0.9
+
+console.log("el precio cond escuento es " + precio)
+
+
+//Operaciones Aritméticas
+
+let cantidad1 = 15
+let cantidad2 = 7
+
+console.log(cantidad1 + cantidad2)
+console.log(cantidad2 - cantidad1)
+console.log(cantidad1 * cantidad2)
+console.log(cantidad1 / cantidad2)
+console.log(cantidad1 % cantidad2) 
+
+
+
+
---
+++ clase-14-/script.js
@@ -0,0 +1,23 @@
+//Condiciones
+
+console.log("el programa inicio correctamente")
+
+let es_administrador = false
+
+if(es_administrador==true){
+    console.log('hola administrador')  
+}
+else {console.log('hola usuario normal')
+
+}
+
+let nombre = '' //es valor falsy
+
+if(nombre){
+    console.log('Vos te llamas ' + nombre)
+}
+else{
+    console.log('No tenes nombre')
+}
+
+    
